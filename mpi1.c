c83b789a654342a00a7f3e87a940ae1273b34ac9
Ubuntu <ubuntu@ip-172-31-19-205.ec2.internal>
2026-09-04 20:59:51 +0000

    .

--- mpi1.c
+++ mpi1.c
@@ -6,10 +6,11 @@ int main(void) {
     int comm_sz;       /* Número de processos */
     int my_rank;       /* Rank do processo */
 
-    int a = 2;
-    int b = 3;
-    int temp;
+    int a = 1;
+    int b = 5;
+    int temp = 0;
     int somador = 0;
+    
     /*
      * Rank 0 --- Soma tudo
      * Rank 1 --- b + a
@@ -29,23 +30,23 @@ int main(void) {
     switch (my_rank) {
 
         case 0:
+
             /* Processo centralizador */
             printf("Processo centralizador em execução: %d de %d!\n", my_rank, comm_sz);
 
             for (int q = 1; q < comm_sz; q++) {
                 MPI_Recv(&temp, 1, MPI_INT, q, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
                 printf("O processo %d enviou o resultado: %d\n", q, temp);
-                somador =+ temp;
-                
+                somador = somador + temp;
+                printf("Somador atual: %d\n", somador);
             }
 
             /* Processo 0 executa a soma, somando o que já tinha */
             printf("O processo %d somou tudo e deu = %d\n", my_rank, somador);
-            
             break;
 
         case 1:
+
             /* Processo 1 executa b + a */
             temp = b + a;
             printf("O processo %d executou: b + a = %d\n", my_rank, temp);
@@ -53,6 +54,7 @@ int main(void) {
             break;
 
         case 2:
+
             /* Processo 2 executa a * b */
             temp = a * b;
             printf("O processo %d executou: a * b = %d\n", my_rank, temp);
@@ -60,11 +62,13 @@ int main(void) {
             break;
 
         case 3:
+
             /* Processo 3 executa a - b */
             temp = a - b;
             printf("O processo %d executou: a - b = %d\n", my_rank, temp);
             MPI_Send(&temp, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
             break;
+
     }
 
     /* Encerra MPI */
